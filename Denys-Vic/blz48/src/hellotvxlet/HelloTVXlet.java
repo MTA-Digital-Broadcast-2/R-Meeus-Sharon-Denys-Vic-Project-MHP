488ba067e15c278b593be981c0b93056e0cb3747
vicdenys <vicdenys@hotmail.com>
2015-05-14 21:24:34 +0200

    oef blz 48

---
+++ Denys-Vic/blz48/src/hellotvxlet/HelloTVXlet.java
@@ -0,0 +1,98 @@
+package hellotvxlet;
+
+import javax.tv.xlet.*;
+import org.dvb.ui.*;
+import org.dvb.event.*;
+import java.util.TimerTask;
+import org.havi.ui.*;
+import org.havi.ui.event.*;
+import java.awt.event.*;
+
+import java.util.Timer;
+
+
+public class HelloTVXlet implements Xlet, UserEventListener {
+
+    private XletContext actueleXletContext;
+    private HScene scene;
+    private boolean debug = true;
+    
+    private MijnComponent cm, cm2;
+  
+    public HelloTVXlet() {
+        
+    }
+
+    public void initXlet(XletContext context) {
+        this.actueleXletContext = context;
+        
+        HSceneTemplate sceneTemplate = new HSceneTemplate();
+        
+        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);
+        sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint(0.0f, 0.0f), HSceneTemplate.REQUIRED);
+    
+        scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
+        
+        cm = new MijnComponent("sterren.png", 0, 0);
+        cm2 = new MijnComponent("sterren.png" , 0, 285);
+        scene.add(cm2);
+        scene.add(cm); 
+        
+        MijnTimerTask mtt = new MijnTimerTask();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(mtt, 0, 24);
+       
+    }
+
+    public void startXlet(){
+        EventManager manager = EventManager.getInstance();
+        UserEventRepository repository = new UserEventRepository("Voorbeeld");
+        
+        repository.addKey( org.havi.ui.event.HRcEvent.VK_UP);
+        repository.addKey( org.havi.ui.event.HRcEvent.VK_DOWN);
+        
+        manager.addUserEventListener(this, repository);
+        
+        scene.validate();
+        scene.setVisible(true);
+        
+    }
+    public void pauseXlet() {
+     
+    }
+
+    public void destroyXlet(boolean unconditional) {
+     
+    }
+    
+    public void userEventReceived(org.dvb.event.UserEvent e){
+        if (e.getType() == KeyEvent.KEY_PRESSED){
+            switch(e.getCode()){
+                case HRcEvent.VK_UP:
+                    cm.moveUp(5);
+                    break;
+                case HRcEvent.VK_DOWN:
+                    cm.moveDown(5);
+                    break;
+            }
+        }
+    }
+
+    public class MijnTimerTask extends TimerTask{
+
+        public void run(){
+            cm.CheckYpos();
+            cm2.CheckYpos();
+            cm2.moveUp(1);
+            cm.moveUp(1);
+        }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Denys-Vic/blz48/src/hellotvxlet/MijnComponent.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hellotvxlet;
+
+import org.dvb.ui.*;
+import org.dvb.event.*;
+import org.havi.ui.*;
+import java.awt.*;
+
+public class MijnComponent extends HComponent{
+    
+    private Image bmap;
+    private MediaTracker mtrack;
+    public int xPos, yPos;
+    
+    public MijnComponent(String bitmapnaam, int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.bmap = this.getToolkit().getImage(bitmapnaam);
+        mtrack = new MediaTracker(this);
+        mtrack.addImage(bmap, 0);
+        
+        try{
+            mtrack.waitForAll();
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+        }
+        
+        this.setBounds( xPos, yPos, bmap.getWidth(null), bmap.getHeight(null));
+    }
+    
+    public void moveUp(int q){
+        this.yPos -= q;
+        this.setBounds(xPos, yPos,bmap.getWidth(null), bmap.getHeight(null));
+    }
+    
+    public void moveDown(int q){
+        this.yPos += q;
+        this.setBounds(xPos, yPos,bmap.getWidth(null), bmap.getHeight(null));
+    }
+    
+    public void MoveToBottom(){
+        this.yPos = bmap.getHeight(null);
+        this.setBounds(xPos, yPos,bmap.getWidth(null), bmap.getHeight(null));
+    }
+    
+    public void CheckYpos(){
+        if (this.yPos == 285*-1){
+            this.yPos = 285;
+            this.setBounds(xPos, yPos,bmap.getWidth(null), bmap.getHeight(null));
+        }
+    }
+    
+    
+    public void paint(Graphics g){
+        g.drawImage(bmap, this.xPos, this.yPos, null);
+    }
+    
+}
